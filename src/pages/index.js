56afcd50ccfa13f7ad68ccda51674a0f44e43a57
vulinh64 <nguyenvulinh64@outlook.com>
2025-08-19 14:22:41 +0700

    Export RecentBlogPosts to a component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
 import clsx from "clsx";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
 import Heading from "@theme/Heading";
+import RecentBlogPosts from "../components/RecentBlogPosts";
 import styles from "./index.module.css";
-
-const recentPosts = require("../../.docusaurus/docusaurus-plugin-content-blog/default/blog-post-list-prop-default.json");
-
-const MAX_RECENT_POST = 6;
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -20,72 +16,6 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
       </div>
     </header>
-  );
-}
-
-function BlogPostItem({ title, date, permalink }) {
-  const formattedDate = date
-    ? new Date(date).toISOString().split("T")[0] // yyyy-MM-dd
-    : null;
-
-  return (
-    <div>
-      <Link
-        to={permalink}
-        className={clsx("text--no-decoration", styles.blogPostItem)}
-      >
-        <div className={styles.blogPostTitle}>{title}</div>
-        {date && (
-          <div className={clsx("subtitle", styles.blogPostDate)}>
-            {formattedDate}
-          </div>
-        )}
-      </Link>
-    </div>
-  );
-}
-
-function RecentBlogPosts() {
-  const items = recentPosts?.items?.filter((item) => !item.unlisted) ?? [];
-
-  if (items.length === 0) {
-    return (
-      <section className="container margin-vert--lg text--center">
-        <Heading as="h1">No blog posts yet 🚧</Heading>
-      </section>
-    );
-  }
-
-  return (
-    <section className="container margin-vert--lg">
-      <div className="row">
-        <div className="col col--12">
-          <Heading as="h1" className="margin-bottom--lg text--center">
-            Recent Blog Posts
-          </Heading>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col col--8 col--offset-3">
-          {items.slice(0, MAX_RECENT_POST).map(({ title, date, permalink }, index) => (
-            <BlogPostItem
-              key={index}
-              title={title}
-              date={date}
-              permalink={permalink}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="text--center margin-top--lg">
-        <Link
-          to="/blog"
-          className="button button--outline button--primary button--lg"
-        >
-          View All Posts
-        </Link>
-      </div>
-    </section>
   );
 }
 
