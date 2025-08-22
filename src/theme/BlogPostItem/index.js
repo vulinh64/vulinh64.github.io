4db8bf2ccad261641d5c82d315f539f014a9d4f6
vulinh64 <nguyenvulinh64@outlook.com>
2025-08-22 11:28:31 +0700

    Test Disqus comment

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -6,6 +6,7 @@ import BlogPostItemHeader from '@theme/BlogPostItem/Header';
 import BlogPostItemContent from '@theme/BlogPostItem/Content';
 import BlogPostItemFooter from '@theme/BlogPostItem/Footer';
 import CustomBlogThumbnail from "../../components/CustomBlogThumbnail";
+import {DiscussionEmbed} from 'disqus-react';
 
 // apply a bottom margin in list view
 function useContainerClassName() {
@@ -15,17 +16,30 @@ function useContainerClassName() {
 
 export default function BlogPostItem({children, className}) {
     const containerClassName = useContainerClassName();
+    const frontMatter = useBlogPost().metadata.frontMatter;
 
-    const thumbnail = useBlogPost().metadata?.frontMatter?.thumbnail;
+    const {slug, title} = frontMatter;
+    const {comment = true} = frontMatter;
 
     return (
         <BlogPostItemContainer className={clsx(containerClassName, className)}>
             <BlogPostItemHeader/>
             <BlogPostItemContent>
-                {!containerClassName && <CustomBlogThumbnail filename={thumbnail}/>}
+                {!containerClassName && <CustomBlogThumbnail filename={frontMatter.thumbnail}/>}
                 {children}
             </BlogPostItemContent>
             <BlogPostItemFooter/>
+
+            <hr />
+
+            {comment && <DiscussionEmbed
+                shortname='vulinhjava'
+                config={{
+                    url: slug,
+                    identifier: slug,
+                    title: title,
+                    language: 'en_US',
+                }}/>}
         </BlogPostItemContainer>
     );
 }
