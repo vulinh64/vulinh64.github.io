38c367b66b65eb83db310f41afcac05bab0e8c60
vulinh64 <nguyenvulinh64@outlook.com>
2025-08-27 10:19:05 +0700

    Modify CSS

--- src/components/QuestionAnswerBox/index.tsx
+++ src/components/QuestionAnswerBox/index.tsx
@@ -1,55 +1,55 @@
-import React, { ReactElement } from "react";
+import React, {ReactElement} from "react";
 import clsx from "clsx";
 import styles from "./styles.module.css";
 
 type QuestionProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
-const Question: React.FC<QuestionProps> = ({ children }) => {
-  return <div className={styles.question}>{children}</div>;
+const Question: React.FC<QuestionProps> = ({children}) => {
+    return <div className={styles.question}>{children}</div>;
 };
 
-const Answer: React.FC<QuestionProps> = ({ children }) => {
-  return (
-    <div className={styles.answer}>
-      <span>{children}</span> {/* Wrap in span to prevent <p> */}
-    </div>
-  );
+const Answer: React.FC<QuestionProps> = ({children}) => {
+    return (
+        <div className={styles.answer}>
+            <span>{children}</span> {/* Wrap in span to prevent <p> */}
+        </div>
+    );
 };
 
 type QuestionAnswerBoxProps = {
-  children:
-    | [ReactElement<typeof Question>, ReactElement<typeof Answer>]
-    | [ReactElement<typeof Answer>, ReactElement<typeof Question>]; // allow either order
+    children:
+        | [ReactElement<typeof Question>, ReactElement<typeof Answer>]
+        | [ReactElement<typeof Answer>, ReactElement<typeof Question>]; // allow either order
 };
 
 const QuestionAnswerBox: React.FC<QuestionAnswerBoxProps> = ({
-  children
-}) => {
-  const childrenArray = React.Children.toArray(children) as ReactElement[];
+                                                                 children
+                                                             }) => {
+    const childrenArray = React.Children.toArray(children) as ReactElement[];
 
-  const question = childrenArray.find(
-    (child) => child.type === Question
-  ) as ReactElement<typeof Question> | undefined;
+    const question = childrenArray.find(
+        (child) => child.type === Question
+    ) as ReactElement<typeof Question> | undefined;
 
-  const answer = childrenArray.find(
-    (child) => child.type === Answer
-  ) as ReactElement<typeof Answer> | undefined;
+    const answer = childrenArray.find(
+        (child) => child.type === Answer
+    ) as ReactElement<typeof Answer> | undefined;
 
-  return (
-    <p>
-      <details className={clsx(styles.details)}>
-        <summary className={clsx(styles.summary)}>
-          <span className={styles.questionIcon}>🤔</span>
-          {question}
-        </summary>
-        <div className={styles.answerContent}>
-          <span className={styles.answer}>{answer}</span>
-        </div>
-      </details>
-    </p>
-  );
+    return (
+        <p>
+            <details className={clsx(styles.details)}>
+                <summary className={clsx(styles.questionBox)}>
+                    <span className={styles.questionIcon}></span>
+                    <span className={styles.question}>{question}</span>
+                </summary>
+                <div className={styles.answerContent}>
+                    <span className={styles.answer}>{answer}</span>
+                </div>
+            </details>
+        </p>
+    );
 };
 
-export { QuestionAnswerBox, Question, Answer };
+export {QuestionAnswerBox, Question, Answer};
